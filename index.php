166912fd192dc04f04d19482bfd1d5674838468e
Jordan <bergerjordan24@gmail.com>
2019-03-01 13:12:07 +0100

    Ajout et mise en forme du menu

---
+++ index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="public/css/style.css">
+        <script src='https://code.jquery.com/jquery-3.3.1.min.js'></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+        <title>Billet simple pour l'Alaska</title>
+    </head>
+    <body>
+        <header id="header">
+            <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#page-top">Billet simple pour l'Alaska</a>
+                    </div>
+                    <div class="collapse navbar-collapse">
+                        <ul class="nav navbar-nav">
+                            <li><a href="?">Accueil</a></li>
+                            <li><a href="?">Articles</a></li>
+                            <li><a href="?">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>   
+        </header>
+        <div id="content">
+        </div>
+        <footer id="footer">
+        </footer>
+        
+    </body>
+</html>

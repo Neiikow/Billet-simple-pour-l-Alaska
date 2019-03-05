12a717cdecaba576b1b820f819e5d30f566cd637
Jordan <bergerjordan24@gmail.com>
2019-03-05 16:33:36 +0100

    Restructuration des pages accueil / articles

---
+++ view/accueil.php
@@ -0,0 +1,19 @@
+<?php $title = "Accueil"; ?>
+
+<?php ob_start(); ?>
+    <button class="btn btn-outline-light" type="button" data-toggle="modal" data-target="#modalLogin">Connexion</button>
+<?php $nav = ob_get_clean(); ?>
+
+<?php ob_start(); ?>
+    <section class="jumbotron">
+        <p>Message de présentation du site</p>
+    </section>
+    <h2>Dernier article</h2>
+    <section class="jumbotron">
+        <p><strong><?= htmlspecialchars($post['author']) ?></strong> le <?= $post['date_post'] ?></p>
+        <h2><?= htmlspecialchars($post['title']) ?></h2>
+        <p><?= nl2br(htmlspecialchars($post['text'])) ?></p>
+    </section>
+<?php $content = ob_get_clean(); ?>
+
+<?php require('view/template.php'); ?>
---
+++ view/articles.php
@@ -0,0 +1,22 @@
+<?php $title = "Articles"; ?>
+
+<?php ob_start(); ?>
+    <button class="btn btn-outline-light" type="button" data-toggle="modal" data-target="#modalLogin">Connexion</button>
+<?php $nav = ob_get_clean(); ?>
+
+<?php ob_start(); ?>
+    <?php
+    while ($post = $posts->fetch())
+    {
+    ?>
+    <section class="jumbotron">
+        <p><strong><?= htmlspecialchars($post['author']) ?></strong> le <?= $post['date_post'] ?></p>
+        <h2><?= htmlspecialchars($post['title']) ?></h2>
+        <p><?= nl2br(htmlspecialchars($post['text'])) ?></p>
+    </section>
+    <?php
+    }
+    ?>
+<?php $content = ob_get_clean(); ?>
+
+<?php require('view/template.php'); ?>

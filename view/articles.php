e3a5cd06128f4849ed321adf645f92f41c26b859
Jordan <bergerjordan24@gmail.com>
2019-03-09 12:58:34 +0100

    Remise en forme de l'affichage des commentaires

--- view/articles.php
+++ view/articles.php
@@ -13,37 +13,45 @@
     <section class="jumbotron post">
         <h3><?= htmlspecialchars($post->title()) ?></h3>
         <p><?= nl2br(htmlspecialchars($post->text())) ?></p>
-        <nav class="navbar navbar-expand-lg navbar-light" role="navigation">
-            <div class="container">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
-                        <a class="nav-item nav-link" href=""><strong>Commentaire(s)</strong></a>
-                        <a class="nav-item nav-link" href=""><strong>Editer</strong></a>
-                        <a class="nav-item nav-link" href=""><strong>Supprimer</strong></a>
-                    </div>
-                </div>
-                <p class="date justify-content-end font-italic"><strong><?= htmlspecialchars($post->author()) ?></strong> le <?= $post->datePost() ?></p>
+        <div class="d-flex justify-content-between align-items-center flex-wrap">
+            <div class="d-flex flex-nowrap">
+                <button type="button" class="btn btn-link btn-sm"><strong>Commentaire(s)</strong></button>
+                <button type="button" class="btn btn-link btn-sm"><strong>Editer</strong></button>
+                <button type="button" class="btn btn-link btn-sm"><strong>Supprimer</strong></button>
             </div>
-        </nav>
-        <?php
-        if (isset($comments)) {
-            foreach ($comments as $comment)
-            { 
-                if ($comment->postId() === $post->id())
+            <p class="btn-sm date font-italic"><strong><?= htmlspecialchars($post->author()) ?></strong> le <?= $post->datePost() ?></p>
+        </div>
+        <section class="container comments table-responsive">
+            <?php
+            if (isset($comments)) {
+            ?>
+            <table class="table table-striped table-bordered">
+                <tbody>
+            <?php
+                foreach ($comments as $comment)
                 {
-        ?>
-        <section class="jumbotron comment">
-            <p><strong><?= htmlspecialchars($comment->author()) ?></strong> le <?= $comment->datePost() ?></p>
-            <p><?= nl2br(htmlspecialchars($comment->text())) ?></p>
-        </section>
-        <?php
+                    if ($comment->postId() === $post->id())
+                    {
+            ?>
+                    <tr class="row">
+                        <td class="col-md-2 text-center pl-1 pr-1">
+                            <p><strong><?= htmlspecialchars($comment->author()) ?></strong></p>
+                            <p class="date"> le <?= $comment->datePost() ?></p>
+                        </td>
+                        <td class="col">
+                            <p><?= nl2br(htmlspecialchars($comment->text())) ?></p>
+                        </td>
+                    </tr>
+            <?php
+                    }
                 }
+            ?>
+                </tbody>
+            </table>
+            <?php
             }
-        }
-        ?>
+            ?>
+        </section>
     </section>
     <?php
     }
